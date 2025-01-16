f2f2978c0766e0cd3fef55d359ba4e784845ae00
Martin Knudsen <martin.knudsen@gmail.com>
2025-01-16 08:15:06 +0000

    new logsmith release

--- Casks/logsmith.rb
+++ Casks/logsmith.rb
@@ -1,7 +1,7 @@
 cask "logsmith" do
-  version "7.0.0"
+  version "8.0.1"
   homepage "https://github.com/otto-de/logsmith"
-  sha256 "b08f09591db08941643bc3bf2a2e05dc68803c758be0ef7cece6ac8cebbcf68d"
+  sha256 "22fba491863db07668e9ce850c14a7e4397662f5c69149658a89390526d4ea87"
   url "https://github.com/otto-de/logsmith/releases/download/#{version}/logsmith_darwin_#{version}.zip"
   app "logsmith.app"
 end
