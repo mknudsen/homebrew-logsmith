00923d681a339f71889108cb8b99c4c856ef8b65
Martin Knudsen <martin.knudsen@otto.de>
2026-01-05 08:09:02 +0100

    Enhance deprecation notices with additional information
    Updated deprecation and disable messages to include links.

--- Casks/logsmith.rb
+++ Casks/logsmith.rb
@@ -4,6 +4,6 @@ cask "logsmith" do
   sha256 "e11b5b2c1e3d7867a7cb21f1dbd14a7481650c5b7b0a97931c8587d56351b7f3"
   url "https://github.com/otto-de/logsmith/releases/download/#{version}/logsmith_darwin_#{version}.zip"
   app "dist/logsmith.app"
-  deprecate! date: "2026-01-05", because: "is now distributed directly by the author.", replacement_cask: "redvox/logsmith"
-  disable! date: "2026-07-05", because: "is now distributed directly by the author.", replacement_cask: "redvox/logsmith" 
+  deprecate! date: "2026-01-05", because: "is now distributed directly by the author. See https://github.com/redvox/homebrew-logsmith "
+  disable! date: "2026-07-05", because: "is now distributed directly by the author. See https://github.com/redvox/homebrew-logsmith "
 end
